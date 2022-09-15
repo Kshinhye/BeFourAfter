23fbb2848a6b4485b0345d60b89c80d43b2b0472
yunhyunsung <gustjd5624@naver.com>
2022-09-15 14:17:58 +0900

    dao

--- BeFourAfter/src/main/java/com/gura/lug/users/dao/UsersDao.java
+++ BeFourAfter/src/main/java/com/gura/lug/users/dao/UsersDao.java
@@ -1,5 +1,14 @@
 package com.gura.lug.users.dao;
 
-public interface UsersDao {
+import com.gura.lug.users.dto.UsersDto;
 
+public interface UsersDao {
+	
+	public boolean isExist(String inputId);
+	public void insert(UsersDto dto);
+	public UsersDto getData(String id);
+	public void update(UsersDto dto);
+	public void delete(String id);
+	public void updatePwd(UsersDto dto);
+	
 }
